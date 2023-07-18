946b878f8c71be3b73ca3102b0d27a146d7322c3
mayu-izumi-epkotsoftware <mayu.izumi@epkotsoftware.co.jp>
2023-07-18 18:06:12 +0900

    namespace名前空間の課題

---
+++ docker/php/kadai1/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use Carbon\Carbon; // ← ここにに入れる名称を答えなさい。
+
+class HomeController
+{
+    public function run()
+    {
+        $now = new Carbon(); // 定義したクラスを使用
+    }
+}
---
+++ docker/php/kadai3/DatabaseSeeder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Database\Seeders;
+
+class DatabaseSeeder
+{
+    public function run()
+    {
+        $adminSeeder = new AdminSeeder(); // 定義されたクラスを使用
+    }
+}
